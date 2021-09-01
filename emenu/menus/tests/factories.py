b67e3b68fbb848d871b49299757f956a11661f71
Adam <adammisiak3@gmail.com>
2021-09-01 17:25:57 +0200

    Added factories and first tests

---
+++ emenu/menus/tests/factories.py
@@ -0,0 +1,26 @@
+import factory
+from factory import fuzzy
+from faker import Faker
+from menus.models import Dish, Menu
+
+faker = Faker(["pl_PL"])
+
+
+class MenuFactory(factory.Factory):
+    class Meta:
+        model = Menu
+
+    name = factory.LazyFunction(lambda: faker.text(max_nb_chars=50))
+    description = factory.LazyFunction(lambda: faker.text(max_nb_chars=1000))
+
+
+class DishFactory(factory.Factory):
+    class Meta:
+        model = Dish
+
+    name = factory.LazyFunction(lambda: faker.text(max_nb_chars=50))
+    description = factory.LazyFunction(lambda: faker.text(max_nb_chars=1000))
+    price = fuzzy.FuzzyInteger(5, 200)
+    photo = factory.django.FileField(filename="test.jpg")
+    prepare_time = fuzzy.FuzzyInteger(5, 60)
+    vegan = faker.pybool()
--- emenu/menus/tests/test_permissions.py
+++ emenu/menus/tests/test_permissions.py
@@ -9,24 +9,14 @@ class MenuTestCase(APITestCase):
         self.user = User.objects.create(username="admin", password="admin")
         self.menu = MenuFactory()
         self.menu.save()
-        self.menu2 = MenuFactory()
-        self.menu2.save()
-        self.menu3 = MenuFactory()
-        self.menu3.save()
         self.dish = DishFactory(menu=self.menu)
         self.dish.save()
         self.dish2 = DishFactory(menu=self.menu)
         self.dish2.save()
-        self.dish3 = DishFactory(menu=self.menu)
-        self.dish3.save()
-        self.dish4 = DishFactory(menu=self.menu)
-        self.dish4.save()
 
-    def test_get_non_null_menus_by_logged_user(self):
+    def test_get_non_null_menus_by_anon_user(self):
         url = reverse("api:menus-list")
-        self.client.force_authenticate(user=self.user)
+        self.client.logout()
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()["results"]), 1)
-        self.assertEqual(response.json()["results"][0]["id"], self.menu.id)
